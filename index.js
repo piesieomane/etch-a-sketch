d56affd0b9c33f78cc2b4d4f67e5c4db7cb6743d
piesieomane <piesieyeboah@gmail.com>
2022-05-19 14:56:06 +0300

    created the grid box

--- index.js
+++ index.js
@@ -2,15 +2,21 @@ let noGrid = 16;
 
 
 
-function createDiv() {
-    var gridDiv = document.createElement("div");
+function createDiv() {  
     var gridDiv2 = document.getElementById("gridDiv");
+    var gridDiv = document.createElement("div");
     
-    for (let i = 0; i < noGrid; i++) {
-        gridDiv.innerHTML = "<div style = 'height:30px; width: 30px; background-color: red; border: 2px solid Black'> hi </div>";
-        gridDiv2.appendChild(gridDiv);
-        console.log("i");
-    }
+    gridDiv2.style.gridTemplateColumns = 'repeat(16,1fr)' ;
+    gridDiv2.style.gridTemplateRows = 'repeat(16,1fr)';
+    gridDiv2.style.gridTemplateRows.border = '2px solid red';
+    gridDiv2.style.gridTemplateColumns.border = '2px solid red'
+    
+    gridDiv2.style.border = "2px solid red";
+    
+    //gridDiv.innerHTML = "<div style = 'height:30px; width: 30px; background-color: red; border: 2px solid Black'> hi </div>";
+       // gridDiv2.appendChild(gridDiv);
+      //  console.log("i");
+
 }
 function handleClick( mythis ) {
     createDiv();
