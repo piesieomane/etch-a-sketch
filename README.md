ea5c50b581381ad98499c81efe02dfb5bae0dd7d
piesieomane <piesieyeboah@gmail.com>
2022-05-19 17:03:08 +0300

    second commit

--- README.md
+++ README.md
@@ -6,4 +6,12 @@
 
 3. Collaborator is Vitalis.
 
-4. Project is mainly of using Javascript to manipulate the DOM to geberate a sketch pad for drawing
+4. Project is mainly of using Javascript to manipulate the DOM to generate a sketch pad for drawing.
+
+5. Create a Big Div that will have the sketch in it
+
+6. Used gridtemplatecolumn and rows to divide the big div into rows and column.
+
+7. create a div and then loop inside the big div to generate smaller divs
+
+8. Now use the evenlistener on the smaller divs to generate a different color
--- index.js
+++ index.js
@@ -3,25 +3,39 @@ let noGrid = 16;
 
 
 function createDiv() {  
-    var gridDiv2 = document.getElementById("gridDiv");
-    var gridDiv = document.createElement("div");
+    let gridDiv2 = document.getElementById("gridDiv");
+    
     
     gridDiv2.style.gridTemplateColumns = 'repeat(16,1fr)' ;
     gridDiv2.style.gridTemplateRows = 'repeat(16,1fr)';
     gridDiv2.style.gridTemplateRows.border = '2px solid red';
-    gridDiv2.style.gridTemplateColumns.border = '2px solid red'
+    gridDiv2.style.gridTemplateColumns.border = '2px solid red';
     
-    gridDiv2.style.border = "2px solid red";
+    // gridDiv.style.backgroundColor = "orange";
+   // gridDiv.innerHTML = "<div style = 'height:30px; width: 30px; background-color: red; border: 2px solid Black'> hi </div>";
     
-    //gridDiv.innerHTML = "<div style = 'height:30px; width: 30px; background-color: red; border: 2px solid Black'> hi </div>";
-       // gridDiv2.appendChild(gridDiv);
-      //  console.log("i");
+      
+    for (let i = 0; i < 256; i++){
+        let gridDiv = document.createElement("div");
+        gridDiv.innerHTML = "<div style = 'background-color: red; width:100%; height:100%; border: 2px solid Black'>  </div>";
+        gridDiv2.appendChild(gridDiv);
+     
+        //Houver over our DIVS
+        gridDiv.addEventListener('mouseover', () => {
+            gridDiv.innerHTML = "<div style = 'background-color: yellow; width:100%; height:100%; border: 2px solid Black'>  </div>";
+        })
+    }
+
+
+
+
 
 }
 function handleClick( mythis ) {
     createDiv();
-    let size = mythis.id;
-    alert(size)
+    //Testing our form//
+   // let size = mythis.id;
+   // alert(size)
     
 }
  
